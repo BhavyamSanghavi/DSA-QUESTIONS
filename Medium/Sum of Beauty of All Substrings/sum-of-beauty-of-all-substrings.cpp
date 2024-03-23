aca41c035696c75a6ee053ed11caca03aad2d32f
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2024-03-23 18:19:19 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sum of Beauty of All Substrings/sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int beautySum(string s) {
+        int ans=0;
+        for(int i=0;i<s.size();i++)
+        {
+            int arr[26]={0};
+            for(int j=i;j<s.size();j++)
+            {
+                arr[s[j]-'a']++;
+                int maxi=0, mini=INT_MAX;
+                
+                for(int k=0;k<26;k++)
+                {
+                    if(arr[k]>0)
+                    {
+                        maxi=max(maxi,arr[k]);
+                        mini=min(mini,arr[k]);
+                    }
+                }
+                ans+=(maxi-mini);
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution obj;
+        cout << obj.beautySum(s) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
