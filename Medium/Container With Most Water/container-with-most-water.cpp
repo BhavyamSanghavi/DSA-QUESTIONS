cf1b0ffc16634dad41091b61237b8484eab736e1
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2024-04-09 19:33:59 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Container With Most Water/container-with-most-water.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+// int maxArea(int A[], int len);
+
+// } Driver Code Ends
+//User function template for C++
+
+long long maxArea(long long A[], int len)
+{
+    int l=0,r=len-1;
+    long long ans=0;
+    while(l<r)
+    {
+        long long water=min(A[l],A[r])*(r-l);
+        ans=max(ans,water);
+        if(A[l]<A[r]) l++;
+        else r--;
+    }
+    return ans;
+}
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+    {
+        int n;
+        cin>>n;
+        long long arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        cout<<maxArea(arr,n)<<endl;
+    }
+return 0;
+}
+
+// } Driver Code Ends
