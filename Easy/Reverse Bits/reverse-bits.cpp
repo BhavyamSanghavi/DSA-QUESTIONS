1b88c39b869091a3790cba3c4d81d7eb63eff4b4
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2024-04-13 10:34:19 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse Bits/reverse-bits.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long reversedBits(long long x) {
+         string str=bitset<32>(x).to_string();
+        reverse(str.begin(),str.end());
+        long long ans=bitset<32>(str).to_ullong();
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long X;
+        
+        cin>>X;
+
+        Solution ob;
+        cout << ob.reversedBits(X) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
