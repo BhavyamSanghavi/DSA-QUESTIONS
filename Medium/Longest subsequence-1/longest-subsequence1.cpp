6c7d19ec7288524770cfcac0d11190ac13bc4f4e
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2024-05-27 00:04:17 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Longest subsequence-1/longest-subsequence1.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int longestSubseq(int N, vector<int> &A) {
+        vector<int>dp(N,1);
+        int res=1;
+        for(int i=0;i<N;i++){
+            for(int j=0;j<i;j++){
+                if(abs(A[i]-A[j])==1){
+                    dp[i]=max(dp[i],dp[j]+1);
+                    res=max(res,dp[i]);
+                }
+            }
+        }
+        return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int n;
+        scanf("%d", &n);
+
+        vector<int> a(n);
+        Array::input(a, n);
+
+        Solution obj;
+        int res = obj.longestSubseq(n, a);
+
+        cout << res << endl;
+    }
+}
+
+// } Driver Code Ends
