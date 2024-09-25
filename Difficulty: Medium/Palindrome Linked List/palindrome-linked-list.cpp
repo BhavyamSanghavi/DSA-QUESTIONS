3beeb41f982e19e01ea3807e052aaa335b796d5e
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2024-09-25 16:04:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.cpp
@@ -0,0 +1,141 @@
+//{ Driver Code Starts
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <cmath>
+#include <cstdio>
+#include <ios>
+#include <iostream>
+#include <random>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+class Solution{
+  public:
+    //Function to check whether the list is palindrome.
+    Node* getMid(Node* head){
+        Node* slow = head;
+        Node* fast = head->next;
+        
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    
+    Node* reverse(Node* head){
+        Node* pre = NULL;
+        Node* curr = head;
+        Node* next;
+        
+        while(curr != NULL){
+            next = curr->next;
+            curr->next = pre;
+            
+            pre=curr;
+            curr=next;
+        }
+        return pre;
+    }
+    bool isPalindrome(Node *head)
+    {
+        if(head==NULL || head->next==NULL) return true;
+        Node* mid=getMid(head);
+        Node* temp=mid->next;
+        mid->next=reverse(temp);
+        
+        Node*t1=head;
+        Node*t2=mid->next;
+        while(t2)
+        {
+            if(t1->data != t2->data) return false;
+            t1=t1->next;
+            t2=t2->next;
+        }
+        return true;
+    }
+};
+
+
+
+
+//{ Driver Code Starts.
+
+/* Function to print nodes in a given linked list */
+void printList(struct Node *head) {
+    struct Node *temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        if (arr.empty()) {
+            cout << "empty" << endl;
+            continue;
+        }
+
+        struct Node *head = new Node(arr[0]);
+        struct Node *tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+        Solution ob;
+        if (ob.isPalindrome(head))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+
+        // Clean up the remaining nodes to avoid memory leaks
+        while (head != NULL) {
+            struct Node *temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+    return 0;
+}
+
+// } Driver Code Ends
