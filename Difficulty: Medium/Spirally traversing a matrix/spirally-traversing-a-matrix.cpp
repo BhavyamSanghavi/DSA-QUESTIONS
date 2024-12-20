0b93dcdff28e7d76e9ec4df1c2c6610da0ec65fe
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2024-12-20 18:36:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.cpp
+++ Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.cpp
@@ -51,7 +51,6 @@ class Solution {
     sc++;
 }return ans;
     }
-   
 };
 
 //{ Driver Code Starts.
@@ -62,9 +61,10 @@ int main() {
     while (t--) {
         int r, c;
         cin >> r >> c;
-        vector<vector<int>> matrix(r, vector<int>(c, 0));
+        vector<vector<int>> matrix(r);
 
         for (int i = 0; i < r; i++) {
+            matrix[i].assign(c, 0);
             for (int j = 0; j < c; j++) {
                 cin >> matrix[i][j];
             }
@@ -75,6 +75,9 @@ int main() {
         for (int i = 0; i < result.size(); ++i)
             cout << result[i] << " ";
         cout << endl;
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
