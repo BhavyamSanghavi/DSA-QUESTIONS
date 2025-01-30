3633bd9283976cabbc5ddbf51dd637b9693f62c0
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2025-01-30 20:30:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
+++ Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
@@ -4,92 +4,86 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 // User function Template for C++
 
-class Solution{
-public:
-
-bool isSafe(vector<vector<int >>chess,int row,int col,int n){
-        //vertically up
-        for(int i=row-1;i>=0;i--){
-            if(chess[i][col]==1){
-                return false;
-            }
-        }
-        //diagonally left
-        for(int i=row-1,j=col-1;i>=0 && j>=0 ;i--,j--){
-            if(chess[i][j]==1){
-                return false;
-            }
-        }
-        //diagonally right
-        for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++){
-            if(chess[i][j]==1){
-                return false;
-            }
-        }
-        return true;
-    }
-    void Findans(vector<vector<int >>chess,int n,vector<vector<int >>&ans){
-        vector<int>temp;
-          for(int i=0;i<n;i++){
-              for(int j=0;j<n;j++){
-                                if(chess[i][j]==1){
-                  temp.push_back(j+1);
-              }
-              }
+class Solution {
+  public:
+  bool isSafe(vector<vector<char>>& mat, int row, int col)
+  {
+      int i=row,j=col;
+      while(j>=0)
+      {
+          if(mat[i][j--]=='Q') return 0;
+      }
+      i=row,j=col;
+      while(i>=0 && j>=0)
+      {
+          if(mat[i--][j--]=='Q') return 0;
+      }
+      i=row,j=col;
+      while(j>=0 && i<mat.size())
+      {
+          if(mat[i++][j--]=='Q') return 0;
+      }
+      return 1;
+  }
+  void solve(vector<vector<int>>& ans, vector<int>& curr, vector<vector<char>>& mat,int row,int col, int n)
+  {
+      if(col==n)
+      {
+          ans.push_back(curr);
+          return;
+      }
+      
+      for(int i=0;i<n;i++)
+      {
+          if(isSafe(mat,i,col))
+          {
+              mat[i][col]='Q';
+              curr.push_back(i+1);
+              solve(ans,curr,mat,0,col+1,n);
+              curr.pop_back();
+              mat[i][col]='.';
           }
-          ans.push_back(temp);
-    }
-    
-void solve(int row,vector<vector<int >>chess,int n,vector<vector<int >>&ans){
-    //basecase
-    if(row==n){
-       Findans(chess,n,ans);
-       return;
-    }
-    //recursive call
-    for(int i=0;i<n;i++){
-        if(isSafe(chess,row,i,n)){
-            chess[row][i]=1;
-            solve(row+1,chess,n,ans);
-            //backtracking
-            chess[row][i]=0;
-        }
-    }
-}
-public:
+      }
+  }
     vector<vector<int>> nQueen(int n) {
-     vector<vector<int >>chess(n,vector<int>(n,0));
-     vector<vector<int>>ans;
-     solve(0,chess,n,ans);
-     return ans ;
+        vector<vector<int>>ans;
+        vector<vector<char>>mat(n,vector<char>(n,'.'));
+        vector<int>curr;
+        solve(ans,curr,mat,0,0,n);
+        return ans;
     }
 };
 
 //{ Driver Code Starts.
 
-int main(){
+int main() {
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--) {
         int n;
-        cin>>n;
-        
+        cin >> n;
+
         Solution ob;
         vector<vector<int>> ans = ob.nQueen(n);
-        if(ans.size() == 0)
-            cout<<-1<<"\n";
+        if (ans.size() == 0)
+            cout << -1 << "\n";
         else {
-            for(int i = 0;i < ans.size();i++){
-                cout<<"[";
-                for(int u: ans[i])
-                    cout<<u<<" ";
-                cout<<"] ";
+            sort(ans.begin(), ans.end());
+            for (int i = 0; i < ans.size(); i++) {
+                cout << "[";
+                for (int u : ans[i])
+                    cout << u << " ";
+                cout << "] ";
             }
-            cout<<endl;
+            cout << endl;
         }
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
