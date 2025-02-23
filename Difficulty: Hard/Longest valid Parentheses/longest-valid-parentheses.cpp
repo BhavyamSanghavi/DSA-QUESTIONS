ed42805d0471555322671781436dd7d31db14163
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2025-02-23 22:47:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.cpp
+++ Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.cpp
@@ -6,23 +6,54 @@ using namespace std;
 
 
 // } Driver Code Ends
-// User function Template for C++
 
 class Solution {
   public:
-    int maxLength(string& s){
-        int ans = 0;
-        stack<pair<char,int>> st;
-        for(int i=0;i<s.size();i++){
-            if(s[i] == ')' and st.size() and st.top().first == '(')
+    int maxLength(string& s) {
+        stack<pair<char,int>> stt;
+        vector<pair<int,int>> ans;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='(')
             {
-                st.pop();
-                ans = max(ans,i-(st.size() ? st.top().second : -1));
+                stt.push({'(',i});
             }
             else
-                st.push({s[i],i});
+            {
+                if(stt.size()>0)
+                {
+                    int ind=stt.top().second;
+                    stt.pop();
+                    ans.push_back({ind,i});
+                }
+            }
         }
-        return ans;
+        if(ans.size()==0)return 0;
+        sort(ans.begin(),ans.end());
+        int st=ans[0].first;
+        int ed=ans[0].second;
+        int ct=ed-st+1;
+        for(int i=1;i<ans.size();i++)
+        {
+            int ind1=ans[i].first;
+            int ind2=ans[i].second;
+            if(ind1-ed==1)
+            {
+                ed=ind2;
+                ct=max(ct,ed-st+1);
+            }
+            else if(ind1-ed>1)
+            {
+                st=ind1;
+                ed=ind2;
+                ct=max(ct,ed-st+1);
+            }
+            else if(st>ind1 && ed>ind2)
+            {
+                
+            }
+        }
+        return ct;
     }
 };
 
@@ -37,6 +68,9 @@ int main() {
 
         Solution ob;
         cout << ob.maxLength(S) << "\n";
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
