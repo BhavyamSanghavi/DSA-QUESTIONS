909edab4b67ef86e58927525bc689d82817b629d
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2025-06-10 01:03:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Exactly one swap/exactly-one-swap.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int countStrings(string &S) {
+        // code here
+        int n=S.size();
+        vector<int>count(26);
+        for(int i=0;i<n;i++){
+            count[S[i]-'a']++;
+        }
+        int ans=0;
+        bool flag=0;
+        for(int i=0;i<26;i++){
+            if(count[i]>0){
+                if(count[i]>1 && flag==0){
+                    ans++;
+                    flag=1;
+                }
+                ans+=1ll*(n-count[i])*count[i];
+                n-=count[i];
+            }
+        }
+        return ans;
+    }
+};
