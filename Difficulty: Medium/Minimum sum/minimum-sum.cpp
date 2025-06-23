040fc445d3bd699d1352476b790a67c72ec8b34b
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2025-06-23 22:18:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum sum/minimum-sum.cpp
+++ Difficulty: Medium/Minimum sum/minimum-sum.cpp
@@ -1,98 +1,43 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
-#include<bits/stdc++.h>
+#include<string>
 class Solution {
   public:
+    string addStrings(string num1, string num2) {
+        string result = "";
+        int carry = 0, i = num1.size() - 1, j = num2.size() - 1;
+
+        while (i >= 0 || j >= 0 || carry) {
+            int digit1 = i >= 0 ? num1[i--] - '0' : 0;
+            int digit2 = j >= 0 ? num2[j--] - '0' : 0;
+
+            int sum = digit1 + digit2 + carry;
+            result.push_back((sum % 10) + '0');
+            carry = sum / 10;
+        }
+
+        reverse(result.begin(), result.end());
+        i=0;
+        while(i<result.size())
+            if(result[i]=='0') 
+                result.erase(i,1);
+            else break;
+        return result;
+    }
+
     string minSum(vector<int> &arr) {
-        sort(arr.begin(),arr.end());
-        string s1,s2;
-        for(int i=0;i<arr.size();i++) {
-            if(arr[i]!=0) {
-                if(i%2 == 0) {
-                s1.push_back(arr[i] + '0');
-                }
-                else {
-                    s2.push_back(arr[i]+'0');
-                }
-            }
+        sort(arr.begin(), arr.end());
+        string num1 = "", num2 = "";
+        bool flag = true;
+
+        for (int i : arr) {
+            char n = '0' + i;  // Convert int digit to char
+            if (flag)
+                num1.push_back(n);
+            else
+                num2.push_back(n);
+            flag = !flag;
         }
-        reverse(s1.begin(),s1.end());
-        reverse(s2.begin(),s2.end());
-        string ans;
-        int carry=0,p1=0,p2=0;
-        while(p1<s1.size() && p2<s2.size()) {
-            int d=s1[p1]-'0' + s2[p2]-'0' + carry;
-            if(d>9) {
-                carry=1;
-                d=d%10;
-            }
-            else {
-                carry=0;
-            }
-            ans.push_back(d+'0');
-            p1++;
-            p2++;
-        }
-        while(p1<s1.size()) {
-            int d=s1[p1]-'0' + carry;
-            if(d>9) {
-                carry=1;
-                d=d%10;
-            }
-            else {
-                carry=0;
-            }
-            ans.push_back(d+'0');
-            p1++;
-        }
-        while(p2<s2.size()) {
-            int d=s2[p2]-'0' + carry;
-            if(d>9) {
-                carry=1;
-                d=d%10;
-            }
-            else {
-                carry=0;
-            }
-            ans.push_back(d+'0');
-            p2++;
-        }
-        if(carry == 1) {
-            ans.push_back('1');
-        }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        
+
+        return addStrings(num1, num2);
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> a;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            a.push_back(number);
-        }
-
-        Solution ob;
-        string ans = ob.minSum(a);
-        cout << ans << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
