c5e3dd6275618c3bf1793f9add7263d55bdb08cb
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2025-12-15 18:31:40 +0530

    Add solution for Minimum Moves to Balance Circular Array

---
+++ LeetCode/Minimum-Moves-to-Balance-Circular-Array/Solution.py
@@ -0,0 +1,18 @@
+    def minMoves(self, A: List[int]) -> int:
+        n = len(A)
+        j = -1
+        for i, a in enumerate(A):
+            if a < 0:
+                j = i
+        if j < 0:
+            return 0
+        if sum(A) < 0:
+            return -1
+        res = 0
+        d = 0
+        while A[j] < 0:
+            d += 1
+            storage = A[(j + d) % n] + A[j - d]
+            res += min(-A[j], storage) * d
+            A[j] += storage
+        return res
