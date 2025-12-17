888c056abeea94e988946b5bc75d6171507c0f90
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2025-12-17 19:36:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Overlapping Intervals/overlapping-intervals.cpp
+++ Difficulty: Medium/Overlapping Intervals/overlapping-intervals.cpp
@@ -1,12 +1,7 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
     vector<vector<int>> mergeOverlap(vector<vector<int>>& intervals) {
+        // Code here
         if(intervals.size()==1)return intervals;
         sort(intervals.begin(),intervals.end());
         vector<vector<int>>ans;
@@ -22,30 +17,3 @@ class Solution {
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int n;
-        cin >> n;
-        vector<vector<int>> arr(n);
-        for (int i = 0; i < n; i++) {
-            int x, y;
-            cin >> x >> y;
-            arr[i].push_back(x);
-            arr[i].push_back(y);
-        }
-        Solution obj;
-        vector<vector<int>> ans = obj.mergeOverlap(arr);
-        for (auto i : ans) {
-            for (auto j : i) {
-                cout << j << " ";
-            }
-        }
-        cout << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
