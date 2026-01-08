1a76453f0e597a4194a4ace428a3bf09a36f24fb
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2026-01-08 13:55:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Subarray with k odds/count-subarray-with-k-odds.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+  int atMost(vector<int>&nums,int k)
+  {
+      if(k<0) return 0;
+      int i=0,ans=0;
+      for(int j=0;j<nums.size();j++)
+      {
+          if(nums[j]%2==1) k-=1;
+          while(k<0) 
+          {
+              if(nums[i++]%2==1) k+=1;
+          }
+          ans+=j-i+1;
+      }
+      return ans;
+  }
+    int countSubarrays(vector<int>& nums, int k) {
+        return atMost(nums,k)-atMost(nums,k-1);
+    }
+};
