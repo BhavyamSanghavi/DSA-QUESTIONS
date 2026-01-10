59438736fd260aeb0ffb559a0657267bde1fed1c
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2026-01-10 14:09:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Substrings with K Distinct/substrings-with-k-distinct.cpp
+++ Difficulty: Medium/Substrings with K Distinct/substrings-with-k-distinct.cpp
@@ -1,26 +1,27 @@
 class Solution {
   public:
-  long long solve(string s, int k)
-  {
-  long long ans=0;
-  int i=0,j=0,count=0;
-  vector<int>v(26,0);
-  while(j<s.size())
-  {
-      v[s[j]-'a']++;
-      if(v[s[j]-'a']==1) count++;
-      while(count>k)
+    long long solve(string s, int k)
+    {
+      long long ans=0;
+      int i=0,j=0,count=0;
+      vector<int>v(26,0);
+      while(j<s.size())
       {
-          v[s[i]-'a']--;
-          if(v[s[i]-'a']==0) count--;
-          i++;
+          v[s[j]-'a']++;
+          if(v[s[j]-'a']==1) count++;
+          while(count>k)
+          {
+              v[s[i]-'a']--;
+              if(v[s[i]-'a']==0) count--;
+              i++;
+          }
+          ans+=j-i+1;
+          j++;
       }
-      ans+=j-i+1;
-      j++;
-  }
-  return ans;
-}
+      return ans;
+    }
     int countSubstr(string& s, int k) {
+        // code here
         return solve(s,k)-solve(s,k-1);
     }
 };
