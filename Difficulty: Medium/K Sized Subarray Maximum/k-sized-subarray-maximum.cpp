ae5355a848a5b016fabe598b6e245606124f0b0b
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2026-01-12 00:33:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
@@ -1,66 +1,17 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     vector<int> maxOfSubarrays(vector<int>& arr, int k) {
-        priority_queue<pair<int,int>>q;     //element,index
+        // code here
         vector<int>ans;
-        int n=arr.size();
-        for(int i=0;i<k;i++)
+        priority_queue<pair<int,int>>q;
+        for(int i=0;i<k;i++) q.push({arr[i],i});
+        ans.push_back(q.top().first);
+        for(int i=k;i<arr.size();i++)
         {
             q.push({arr[i],i});
-        }
-        
-        for(int i=0;i<n-k;i++)
-        {
+            while(q.top().second<=i-k) q.pop();
             ans.push_back(q.top().first);
-            while(!q.empty() && i>=q.top().second) q.pop();
-            q.push({arr[i+k],i+k});
         }
-        ans.push_back(q.top().first);
         return ans;
-        
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // Ignore newline character after t
-
-    while (t--) {
-        vector<int> arr;
-        int k;
-        string inputLine;
-
-        getline(cin, inputLine); // Read the array input as a line
-        stringstream ss(inputLine);
-        int value;
-        while (ss >> value) {
-            arr.push_back(value);
-        }
-
-        cin >> k;
-        cin.ignore(); // Ignore newline character after k input
-
-        Solution obj;
-        vector<int> res = obj.maxOfSubarrays(arr, k);
-        for (int i = 0; i < res.size(); i++)
-            cout << res[i] << " ";
-        cout << endl;
-        cout << "~"
-             << "\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
