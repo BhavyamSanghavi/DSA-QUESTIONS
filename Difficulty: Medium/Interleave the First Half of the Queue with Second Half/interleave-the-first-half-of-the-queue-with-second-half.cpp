08b8bf13579121ba1e01384ad39bb854173dcefd
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2026-01-30 10:42:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Interleave the First Half of the Queue with Second Half/interleave-the-first-half-of-the-queue-with-second-half.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    void rearrangeQueue(queue<int> &q) {
+        queue<int>f;
+        
+        int n=q.size()/2;
+        
+        for(int i=0;i<n;i++){
+            f.push(q.front());
+            q.pop();
+        }
+        while(!f.empty()){
+            q.push(f.front());
+            q.push(q.front());
+            f.pop();
+            q.pop();
+        }
+    }
+};
