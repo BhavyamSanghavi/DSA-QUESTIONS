a6b9edcd0195faf4692a43818c645116aa327458
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2026-02-02 13:05:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.cpp
+++ Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.cpp
@@ -1,58 +1,39 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    // arr: input array
-    // Function to find maximum circular subarray sum.
-    int circularSubarraySum(vector<int> &arr) {
-        int maxi=-1e9, mini=1e9;
-        int sum=0,maxsum=0,minsum=0;
-        for(auto i:arr)
-        {
-            maxi=max(maxi,i+maxsum);
-            mini=min(mini,i+minsum);
-            sum+=i;
-            maxsum+=i;
-            minsum+=i;
-            
-            if(minsum>0) minsum=0;
-            if(maxsum<0) maxsum=0;
-        }
-        if(maxi<0) return maxi;
-        return max(maxi,sum-mini);
-        
-        
+    int kadane(const vector<int>& arr) {
+    int maxSoFar = arr[0];
+    int currSum = arr[0];
+
+    for (int i = 1; i < arr.size(); i++) {
+        currSum = max(arr[i], currSum + arr[i]);
+        maxSoFar = max(maxSoFar, currSum);
     }
-};
 
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-
-        // Read first array
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        int res = ob.circularSubarraySum(arr);
-
-        cout << res << endl;
-    }
-    return 0;
+    return maxSoFar;
 }
 
-// } Driver Code Ends
+    int maxCircularSum(vector<int> &arr) {
+        int n = arr.size();
+           
+           int mini = INT_MAX, minsum = 0;
+           int maxi = INT_MIN, maxsum = 0;
+           int total =0;
+           for(int i:arr){
+                total+=i;
+                minsum+=i;
+                maxsum+=i;
+                mini = min(mini, minsum);
+                maxi = max(maxi, maxsum);
+                if(minsum>0){
+                    minsum=0;
+                }
+                if(maxsum<0){
+                    maxsum=0;
+                }
+           }
+           
+          if(maxi<0) return maxi;
+           
+           return max(maxi, total-mini);
+    }
+};
