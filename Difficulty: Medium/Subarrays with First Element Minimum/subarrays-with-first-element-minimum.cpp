c38bbf2e9db32568e93e8dc40fe9402df7f20287
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2026-03-10 23:26:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarrays with First Element Minimum/subarrays-with-first-element-minimum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int countSubarrays(vector<int> &arr) {
+        // code here
+        int ans=0;
+        stack<int>st;
+        
+        for(int i=0;i<arr.size();i++)
+        {
+            while(!st.empty() && arr[st.top()]>arr[i])
+            {
+                ans+=i-st.top();
+                st.pop();
+            }
+            st.push(i);
+        }
+        while(!st.empty())
+        {
+            ans+=arr.size()-st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
