eee45caa6e16ee22742d1fed7819e1628b17b6aa
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2026-05-01 11:29:36 +0530

    Add solution for Minimum Operations to Make Array Non Decreasing

---
+++ LeetCode/Minimum-Operations-to-Make-Array-Non-Decreasing/Solution.cpp
@@ -0,0 +1,7 @@
+    long long minOperations(vector<int>& A) {
+        long long res = 0;
+        for (int i = 0; i + 1 < A.size(); i++) {
+            res += max(0, A[i] - A[i + 1]);
+        }
+        return res;
+    }
