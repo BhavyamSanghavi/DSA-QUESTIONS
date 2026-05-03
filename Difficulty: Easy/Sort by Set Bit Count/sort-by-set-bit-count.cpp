d2a27cfa6b5db980ff0718e2ebe574bcd9c9a0f0
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2026-05-03 11:46:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sort by Set Bit Count/sort-by-set-bit-count.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    vector<int> sortBySetBitCount(vector<int>& arr) {
+        // code here
+        vector<vector<int>> buckets(32);
+
+        for (int num : arr) {
+            buckets[__builtin_popcount((unsigned int)num)].push_back(num);
+        }
+
+        vector<int> ans;
+        ans.reserve(arr.size());
+        for (int bits = 31; bits >= 0; --bits) {
+            for (int num : buckets[bits]) {
+                ans.push_back(num);
+            }
+        }
+
+        return ans;
+    }
+};
