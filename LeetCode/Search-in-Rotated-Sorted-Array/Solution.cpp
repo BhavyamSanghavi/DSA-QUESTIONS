7f6401d5573e402bd9073a8d54b6b41106342904
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2026-05-22 11:35:20 +0530

    Add solution for Search in Rotated Sorted Array

---
+++ LeetCode/Search-in-Rotated-Sorted-Array/Solution.cpp
@@ -0,0 +1,7 @@
+l=0, r=n-1, mid = any_valid_idx
+while(l <= r) {
+    // 1. CHECK : [required condtion] found ? 
+    // 2. PARTITION : decide which half to go next?
+    // 3. UPDATE : midfor next iteration
+}
+return mid;
