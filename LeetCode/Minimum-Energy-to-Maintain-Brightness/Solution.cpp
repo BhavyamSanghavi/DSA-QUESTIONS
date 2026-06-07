47b73780937e5ae444af534d6360083b432f2df5
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2026-06-07 10:29:04 +0530

    Add solution for Minimum Energy to Maintain Brightness

--- LeetCode/Minimum-Energy-to-Maintain-Brightness/Solution.cpp
+++ LeetCode/Minimum-Energy-to-Maintain-Brightness/Solution.cpp
@@ -3,26 +3,28 @@
 3    vector<vector<int>> mergeIntervals(vector<vector<int>>& intervals)
 4    {
 5        sort(intervals.begin(),intervals.end());
-6        vector<vector<int>>res;
-7        res.push_back(intervals[0]);
-8
-9        for(int i=1;i<intervals.size();i++)
-10        {
-11            if(res[res.size()-1][1]>=intervals[i][0])
-12                res[res.size()-1][1]=max(res[res.size()-1][1],intervals[i][1]);
-13            else
-14                res.push_back(intervals[i]);
-15        }
-16        return res;
-17    }
-18    long long minEnergy(int n, int brightness, vector<vector<int>>& intervals) {
-19        int bulbs=ceil(brightness/3.0);
-20        intervals=mergeIntervals(intervals);
-21
-22        int time=0;
-23        for(auto i:intervals)
-24            time+=i[1]-i[0]+1;
-25        
-26        return (long long)bulbs*time;
-27    }
-28};
+6        
+7        vector<vector<int>>res;
+8        res.push_back(intervals[0]);
+9
+10        for(int i=1;i<intervals.size();i++)
+11        {
+12            if(res[res.size()-1][1]>=intervals[i][0])
+13                res[res.size()-1][1]=max(res[res.size()-1][1],intervals[i][1]);
+14            else
+15                res.push_back(intervals[i]);
+16        }
+17        
+18        return res;
+19    }
+20    long long minEnergy(int n, int brightness, vector<vector<int>>& intervals) {
+21        int bulbs=ceil(brightness/3.0);
+22        intervals=mergeIntervals(intervals);
+23
+24        int time=0;
+25        for(auto i:intervals)
+26            time+=i[1]-i[0]+1;
+27        
+28        return (long long)bulbs*time;
+29    }
+30};
