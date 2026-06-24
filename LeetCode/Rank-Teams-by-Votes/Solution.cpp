345d4c705e2742a994e4df89a93dfddd6a54b848
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2026-06-24 10:57:02 +0530

    Add solution for Rank Teams by Votes

---
+++ LeetCode/Rank-Teams-by-Votes/Solution.cpp
@@ -0,0 +1,7 @@
+KeyError: 'Z'
+   count[v][i] -= 1
+Line 17 in rankTeams (Solution.py)
+   ret = Solution().rankTeams(param_1)
+Line 37 in _driver (Solution.py)
+   _driver()
+Line 48 in <module> (Solution.py)
