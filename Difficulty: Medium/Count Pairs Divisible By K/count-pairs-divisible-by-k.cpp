a9670403c98ca094fe435896d670f8c0bf3379e1
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2026-07-09 10:19:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Pairs Divisible By K/count-pairs-divisible-by-k.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int countKdivPairs(vector<int>& arr, int k) {
+        // code here
+        int n = arr.size();
+        int ans = 0;
+        vector<int> mpp(k);
+        for(auto it : arr)
+        {
+            ans += mpp[it%k];
+            mpp[(-it%k + k)%k]++;
+        }
+        return ans;
+    }
+};
