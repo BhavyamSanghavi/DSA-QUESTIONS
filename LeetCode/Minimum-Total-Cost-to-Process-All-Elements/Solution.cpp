65ce182847bda50bdb34fc6963623b9205d9a7ce
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2026-07-14 13:15:13 +0530

    Add solution for Minimum Total Cost to Process All Elements

---
+++ LeetCode/Minimum-Total-Cost-to-Process-All-Elements/Solution.cpp
@@ -0,0 +1,7 @@
+    int minimumCost(vector<int>& A, int k) {
+        long long s = 0, mod = 1000000007;
+        for (int a : A)
+            s += a;
+        long long x = (s + k - 1) / k % mod;
+        return (x - 1) * x / 2 % mod;
+    }
