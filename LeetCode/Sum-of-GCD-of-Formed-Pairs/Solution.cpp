13ac309a43c40d43e5ca25f23e18fbb1a9f2c6a4
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2026-07-16 15:14:46 +0530

    Add solution for Sum of GCD of Formed Pairs

--- LeetCode/Sum-of-GCD-of-Formed-Pairs/Solution.cpp
+++ LeetCode/Sum-of-GCD-of-Formed-Pairs/Solution.cpp
@@ -1,6 +1,23 @@
-int gcd(int a, int b) {
-        if(b == 0 || a == b) return a;
-        LL t = ((LL)a << 30) | b;
-        if(memo.count(t)) return memo[t];
-        return memo[t] = gcd(b, a % b);
-    }
+1class Solution {
+2public:
+3    long long gcdSum(vector<int>& nums) {
+4        vector<int>prefixGcd(nums.size());
+5        int mx=-1;
+6        
+7        for(int i=0;i<nums.size();i++)
+8        {
+9            mx=max(mx,nums[i]);
+10            prefixGcd[i]=gcd(nums[i],mx);
+11        }
+12        
+13        sort(prefixGcd.begin(),prefixGcd.end());
+14        
+15        long long ans=0;
+16        int i=0,j=prefixGcd.size()-1;
+17        
+18        while(i<j)
+19            ans+=gcd(prefixGcd[i++],prefixGcd[j--]);
+20        
+21        return ans;
+22    }
+23};
